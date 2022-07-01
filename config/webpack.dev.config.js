0e90af17f1ee7cadff45858c723bbf8e831f993d
Daniel Bischoff <daniel.bisch01@gmail.com>
2022-07-01 21:06:39 +0200

    Updated deps to newest versions

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,62 +1,60 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
 
-const HOST = process.env.HOST || '127.0.0.1';
-const PORT = process.env.PORT || '9000';
+const HOST = process.env.HOST || "127.0.0.1";
+const PORT = process.env.PORT || "9000";
 
 const config = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    app: './src/index.tsx',
+    app: "./src/index.tsx",
   },
   output: {
-    path: path.resolve(__dirname, '../dist'),
-    filename: '[name].bundle.js'
+    path: path.resolve(__dirname, "../dist"),
+    filename: "[name].bundle.js",
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: [".ts", ".tsx", ".js"],
   },
-  devtool: 'eval-cheap-module-source-map',
+  devtool: "eval-cheap-module-source-map",
   module: {
-    rules: [{
+    rules: [
+      {
         test: /\.tsx?$/,
-        use: [{
-          loader: 'ts-loader',
-          options: {
-            onlyCompileBundledFiles: true,
-          }
-        }]
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              onlyCompileBundledFiles: true,
+            },
+          },
+        ],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 8192
-          }
-        }]
+        type: "asset/resource",
       },
     ],
   },
   devServer: {
     host: HOST,
     port: PORT,
-    compress: true,
-    inline: true,
-    historyApiFallback: true,
     hot: true,
-    overlay: true,
+    compress: true,
+    historyApiFallback: true,
     open: true,
+    client: {
+      overlay: true,
+    },
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      title: 'React Mobx Starter',
-      inject: 'body'
+      template: "src/index.html",
+      title: "React Mobx Starter",
+      inject: "body",
     }),
-  ]
+  ],
 };
 
 module.exports = config;
